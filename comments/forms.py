0d1991431e729e2627bd71af032f5c19268bb0bd
Anshul <athakur@cdot.in>
2016-07-04 10:57:16 +0530

    [WIP] Teaser View and Detail View major sections completed.

--- comments/forms.py
+++ comments/forms.py
@@ -7,17 +7,30 @@ from django import forms
 from models import Comment
 from blogging.models import BlogContent
 
-class CommentForm(forms.ModelForm):
+from django.contrib.auth.models import User
+
+class CommentForm(forms.Form):
     
-    body = forms.Textarea()
+    post = forms.ModelChoiceField(queryset=BlogContent.published.all())
+    body = forms.CharField(widget=forms.Textarea(attrs={'cols': 40,
+    													 'rows': 4,
+    													 'class':"form-control",
+    													 'placeholder':'Enter your comment here...'}),
+    											  required=True)
+
+    author_name = forms.CharField(widget=forms.TextInput(attrs={'class':"form-control",
+    														    'placeholder':'Name'}), required=False)
+    author_email = forms.EmailField(widget=forms.EmailInput(attrs={'class':"form-control",
+    														 	   'placeholder':'e-mail'}), required=False)
+    author_url = forms.URLField(widget=forms.URLInput(attrs={'class':"form-control",
+    														 'placeholder':'www.'}), 
+    														 required=False)
     
-    def __init__(self, *args, **kwargs):
-        
+    parent_comment = forms.ModelChoiceField(queryset=Comment.objects.all())
+
+    def __init__(self, *args, **kwargs):        
         super(CommentForm, self).__init__(*args, **kwargs)
         
         if kwargs.get('initial', None) is not None:
             self.fields['parent_comment'].queryset = Comment.objects.filter(post= kwargs['initial']['post'])
             self.fields['post'].queryset = BlogContent.objects.filter(pk= kwargs['initial']['post'].id)
-    class Meta:
-        model = Comment
-        exclude = ('published', 'date_created', 'date_modified', 'author')
