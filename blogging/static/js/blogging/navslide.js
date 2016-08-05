5dfc1fd921ae08418ba030bb8b0b750f8d1e179f
Anshul Thakur <anshulthakur@rediffmail.com>
2016-08-05 23:49:24 +0530

    Slideout Sidebar on click anywhere.

--- blogging/static/js/blogging/navslide.js
+++ blogging/static/js/blogging/navslide.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 	var showSidebar = function(){
 		$('#siteNavCollapse').addClass('slidein');
+		$('body').on('click', clickHideSidebar);
 		return (false);
 	};
 	
 	var hideSidebar = function(){
 		$('#siteNavCollapse').removeClass('slidein');
+		$('body').off('click', clickHideSidebar);
 		return(false);
 	};
+	
+	var clickHideSidebar = function(e) {
+		  if ($('#siteNavCollapse').hasClass('slidein')) {
+		    hideSidebar();
+		  }
+		  return(false);
+		};
 	
 	$('#navSlide').on('click', showSidebar);
 	$('#navSlideback').on('click', hideSidebar);
